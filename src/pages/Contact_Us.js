c36da2b4fa08bb9c260b9ba60a2ec2f2d94d6860
JeremyManurung <87016966+JeremyManurung@users.noreply.github.com>
2021-09-19 01:09:44 +0700

    Memperbaiki error localStorage

--- src/pages/Contact_Us.js
+++ src/pages/Contact_Us.js
@@ -1,15 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import validasi from "./validasi";
 import { Stress } from './Stress';
-import {useDispatch,useSelector} from "react-redux"
+import {useDispatch} from "react-redux"
 import { tambahData } from '../store/contactSlice';
-import { Link } from 'react-router-dom';
-import Review from './Review';
 
 
 // getting the values of local storage
 const getDatafromLS=()=>{
-  const data = localStorage.getItem('forms');
+  const data = localStorage.getItem('books');
   if(data){
     return JSON.parse(data);
   }
@@ -19,47 +17,15 @@ const getDatafromLS=()=>{
 }
 
 const Contact_Us = () => {
-
-//   const contacts = useSelector((state) => state.contacts.contacts)
-//     const dispatch = useDispatch()
-
-//     const[values,setValue] = useState(contacts)
-
-
-//   const handleSubmit = (e) => {
-//               const newData = {
-//                   firstname: values.firstname,
-//                   email: values.email,
-//                   number: values.number,
-//                   national: values.national,
-//                   message: values.pesan
-//               }
-//               dispatch(tambahData(newData));
-
-//     useEffect(() => {
-//         setValue(
-//             {
-//                 firstname: "",
-//                 email: "",
-//                 number: "",
-//                 national: "",
-//                 pesan: "",
-//             }
-//         )
-//         dispatch(tambahData(values));
-//     },[]);
-
-// //  // main array of objects state || books state || books array of objects
+ // main array of objects state || books state || books array of objects
   const [forms, setForms]=useState(getDatafromLS());
 
   // input field states
-  const [title, setTitle]=useState('');
   const [email, setEmail]=useState('');
-  const [author, setAuthor]=useState('');
   const [Pnumber, setNumber]=useState('');
   const [national, setNational]=useState('');
   const [Ppesan, setPpesan]=useState('');
-  const [firtsname, setFirtsname]=useState('');
+  const [nama, setNama]=useState('');
   
 
   // form submit event
@@ -67,24 +33,74 @@ const Contact_Us = () => {
     e.preventDefault();
     // creating an object
     let Cform={
-      title,
       email,
-      author,
       Pnumber,
       national,
       Ppesan,
-      firtsname
+      nama
     }
     setForms([...forms,Cform]);
   }
 
   // saving data to local storage
   useEffect(()=>{
-    localStorage.setItem('forms',JSON.stringify(forms));
+    localStorage.setItem('books',JSON.stringify(forms));
   },[forms])
 
     
-  
+        // const [values, setValues] = useState({
+        //     firstname : "",
+        //     email : "",
+        //     number : "",
+        //     national : "",
+        //     pesan : ""
+        // });
+
+        // const [errors, setErrors] = useState({});
+
+        // const handleChange = (event) =>{
+        //     setValues({
+        //         ...values,
+        //         [event.target.name]: event.target.value, 
+        //     });
+        // };
+
+        // const handleFormSubmit = (event) => {
+        //     event.preventDefault();
+        //     setErrors(validasi(values));
+        // }
+
+
+    // const dispatch = useDispatch()
+
+    // const [userInput, setUserInput] = useState('')
+
+    // const handleChange = (e) => {
+    //     setUserInput(e.currentTarget.value)
+    // }
+    
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const formIsNotEmpty = userInput === ""
+    //     if(formIsNotEmpty){
+    //         alert("Input Kosong") 
+    //     }
+    //     else{
+    //         const newItem = {
+    //         // id: uuidv4(),
+    //         // task: userInput,
+    //         // complete: false
+    //         id: uuidv4(),
+    //         firstname : "",
+    //         email : "",
+    //         number : "",
+    //         national : "" ,
+    //         pesan : "",
+    //     }
+    //         dispatch(tambahData(newItem))
+    //         setUserInput("") 
+    //     } 
+
 
     return (
     <div className="contact">
@@ -105,12 +121,123 @@ const Contact_Us = () => {
         
         <div className="contact-left m-xl-5">
             <h4>Contact Us</h4>
-        <form autoComplete="off" className='form-group'
+            {/* <form onClick={handleFormSubmit}>
+            <div className="form-group ">
+                <label>
+                First Name
+                <span> * </span>
+                </label>
+
+                <input
+                type="text" 
+                className="form-control" 
+                placeholder="Your Full Name Here..."
+                name="firstname"
+                id="name"
+                value={values.firstname}
+                onChange={handleChange}
+                />
+
+                {errors.firstname && <p className="error">{errors.firstname}</p>}
+            </div>
+
+            <div className="form-group">
+                <label>
+                Email Address 
+                <span> * </span>
+                </label>
+
+                <input 
+                type="email" 
+                className="form-control" 
+                placeholder="Example@domain.com"
+                name ="email"
+                id="email"
+                value={values.email}
+                onChange={handleChange}
+                />
+
+                {errors.email && <p className="error">{errors.email}</p>}
+            </div>
+
+            <div className="form-group">
+                <label>
+                Phone Number
+                <span> * </span>
+                </label>
+
+                <input 
+                type="text" 
+                className="form-control" 
+                placeholder="0857389xxxxx"
+                name="number"
+                id="Pnumber"
+                value={values.number}
+                onChange={handleChange}
+                />
+
+                {errors.number && <p className="error">{errors.number}</p>}
+            </div>
+
+            <div className="form-group">
+                <label>Nationality
+                <span> * </span>
+                </label>
+
+                <select 
+                className="form-control" 
+                id="exampleFormControlSelect1"
+                name="national"
+                id="nationality"
+                value={values.national}
+                onChange={handleChange}
+                >
+
+                <option value="1">Indonesia</option>
+                <option value="2">Jepang</option>
+                <option value="3">Canada</option>
+                <option value="4">German</option>
+                <option value="5">USA</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+            <label>Message</label>
+            <textarea
+            className="form-control"
+            rows="3"
+            name="pesan"
+            id="pesanGan"
+            value={values.pesan}
+            onChange={handleChange}
+            >
+            </textarea>
+        </div>
+
+        <button 
+        type="button" 
+        className="btn btn-alta mt-4"
+        // onClick={handleFormSubmit}
+        style={{
+            background:'orange',
+            color:'white'
+        }}
+        >
+        Submit
+        </button>
+
+            </form> */}
+          <form autoComplete="off" className='form-group'
           onSubmit={handleSubmit}>
-            <label>Title</label>
-            <input type="text" className='form-control' required
-            onChange={(e)=>setTitle(e.target.value)} value={title}></input>
-            <br></br>
+            <label>Nama</label>
+            <input 
+            type="text" 
+            className='form-control'
+            required
+            onChange={(e)=>setNama(e.target.value)} 
+            value={nama}>
+            </input>
+            
             <label>
                 Email Address 
                 <span> * </span>
@@ -121,13 +248,9 @@ const Contact_Us = () => {
                 placeholder="Example@domain.com"
                 name ="email"
                 value={email}
+                required
                 onChange={(e)=>setEmail(e.target.value)}
                 /><br></br>
-
-            <label>Author</label>
-            <input type="text" className='form-control' required
-            onChange={(e)=>setAuthor(e.target.value)} value={author}></input>
-            <br></br>
 
               <label>
                 Phone Number
@@ -140,6 +263,7 @@ const Contact_Us = () => {
                 placeholder="0857389xxxxx"
                 name="number"
                 value={Pnumber}
+                required
                 onChange={(e)=>setNumber(e.target.value)}
                 />
 
@@ -171,33 +295,32 @@ const Contact_Us = () => {
             value={Ppesan}
             onChange={(e)=>setPpesan(e.target.value)}
             />
-            <label>Nama</label>
-            <input type="text" className='form-control' required
-            onChange={(e)=>setFirtsname(e.target.value)} value={firtsname}></input>
+            
             <br></br>
-            {/* <Link to="/Stress"> */}
             <button type="submit" className='btn btn-success btn-md'>
               ADD
             </button>
-            {/* </Link> */}
           </form>
             </div>
 
-  
+        <div id="output">
+
+        </div>
+        
         <div className='view-container'>
           {forms.length>0&&<>
             <div className='table-responsive'>
+      
                   <Stress forms={forms} />
+                
             </div>
             <button className='btn btn-danger btn-md'
-            onClick={()=>setForms([])}>Remove All</button>
+            onClick={()=>setForms([])}>Hapus</button>
           </>}
-          {forms.length < 1 && <div>No books are added yet</div>}
-        </div>  
+          {forms.length < 1 && <div>Kosong</div>}
+        </div>
 </div>
-);
-          }
-
-
+    );
+}
 
 export default Contact_Us
